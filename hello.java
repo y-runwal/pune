9c1f3fbb3faa8a8bcc52da37b84fd73df02f8e38
y-runwal <yuvraj.runwal@mitaoe.ac.in>
2023-09-10 10:34:13 +0530

    1st  commit

---
+++ hello.java
@@ -0,0 +1,9 @@
+class a{
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello");
+                System.out.println("Hfhello");
+                System.out.println("gjHfhello");
+
+    }
+}
